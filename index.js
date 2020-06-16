e77540d7a23dd1a47d9a7c12265d01ba3834046f
Hanan Dlshad <ensw03217@student.su.edu.krd>
2020-06-16 12:10:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,6 +24,10 @@ class Evergreen extends Tree{
      this.name = name
    }
       static definition(){
-     return `${super.definition()} Deciduous trees shed their leaves annually.`
+     return `${super.definition()} Evergreens keep their leaves all year round.`
    }
 }
+
+let tree = new Tree("Maple")
+
+let deciduous = new Deciduous()
